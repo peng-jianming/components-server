e10e4ffb52c501e5de4ddfd2d29dce862dfcd44a
Doyoudo丶 <728494186@qq.com>
2021-03-08 14:29:58 +0800

    修改图片上传地址

--- src/controller/user.js
+++ src/controller/user.js
@@ -55,7 +55,7 @@ class UserController {
       data: {
         url: `${
           process.env.NODE_ENV === 'production'
-            ? 'https://8.129.90.25:3000/upload'
+            ? 'http://8.129.90.25:3000/upload'
             : 'http://localhost:3000/upload'
         }/${basename}`
       }

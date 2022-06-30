2b59aaaf637412cdb1c036d7b3a7d364526475c4
pengjianming <728494186@qq.com>
2022-06-30 11:00:32 +0800

    fix

--- src/controller/user.js
+++ src/controller/user.js
@@ -55,7 +55,7 @@ class UserController {
       data: {
         url: `${
           process.env.NODE_ENV === 'production'
-            ? 'https://www.pengjianming.top/upload'
+            ? 'http://8.129.90.25/upload'
             : 'http://localhost:3000/upload'
         }/${basename}`
       }
